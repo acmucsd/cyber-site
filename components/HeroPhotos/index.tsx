f1d5cf6458be775a0c3e52bc4be21045f0bccdb1
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2024-11-25 17:11:48 -0800

    feat: add alt text, improve image sizes by like 20x

--- components/HeroPhotos/index.tsx
+++ components/HeroPhotos/index.tsx
@@ -15,22 +15,71 @@ import styles from "./style.module.css";
 
 type HeroPhoto = {
 	image: StaticImageData;
+	alt: string;
 	position: { x: number; y: number; size: number };
 };
 
 const photos: HeroPhoto[] = [
-	{ image: KickoffCrowd, position: { x: 4, y: 4, size: 10 } },
-	{ image: RubberDucky, position: { x: 9, y: 2, size: 2 } },
-	{ image: NickKneeling, position: { x: 6, y: 1, size: 3 } },
-	{ image: SamDiamond, position: { x: 4, y: 2, size: 2 } },
-	{ image: LockpickJig, position: { x: 1, y: 4, size: 3 } },
-	{ image: KickoffAudience, position: { x: 0, y: 7, size: 4 } },
-	{ image: RubberDuckyPeople, position: { x: 2, y: 11, size: 2 } },
-	{ image: JayAudience, position: { x: 6, y: 14, size: 3 } },
-	{ image: NickPresent, position: { x: 9, y: 14, size: 3 } },
-	{ image: UnoPlayer, position: { x: 12, y: 14, size: 2 } },
-	{ image: RedRoom, position: { x: 14, y: 11, size: 2 } },
-	{ image: Hike, position: { x: 14, y: 7, size: 4 } },
+	{
+		image: KickoffCrowd,
+		alt: "a crowd surrounds a UCSD kiosk at ACM Cyber Kickoff 2024",
+		position: { x: 4, y: 4, size: 10 },
+	},
+	{
+		image: RubberDucky,
+		alt: "a USB rubber ducky consisting of an Arduino board encased in a light blue 3D-printed case",
+		position: { x: 9, y: 2, size: 2 },
+	},
+	{
+		image: NickKneeling,
+		alt: "Nick demonstrates lockpicking",
+		position: { x: 6, y: 1, size: 3 },
+	},
+	{
+		image: SamDiamond,
+		alt: "a person forms a diamond with their hands",
+		position: { x: 4, y: 2, size: 2 },
+	},
+	{
+		image: LockpickJig,
+		alt: "people picking locks on a jig",
+		position: { x: 1, y: 4, size: 3 },
+	},
+	{
+		image: KickoffAudience,
+		alt: "a room of people at ACM Cyber Kickoff 2022",
+		position: { x: 0, y: 7, size: 4 },
+	},
+	{
+		image: RubberDuckyPeople,
+		alt: "people at a table program the Arduino boards in their USB rubber duckies",
+		position: { x: 2, y: 11, size: 2 },
+	},
+	{
+		image: JayAudience,
+		alt: "a room of people at Cyber Camp 2023",
+		position: { x: 6, y: 14, size: 3 },
+	},
+	{
+		image: NickPresent,
+		alt: "Nick explains types of locks standing next to his presentation",
+		position: { x: 9, y: 14, size: 3 },
+	},
+	{
+		image: UnoPlayer,
+		alt: "a person holds a deck of Uno cards, smiling",
+		position: { x: 12, y: 14, size: 2 },
+	},
+	{
+		image: RedRoom,
+		alt: "a room illuminated in red light of people watching a person on the projector",
+		position: { x: 14, y: 11, size: 2 },
+	},
+	{
+		image: Hike,
+		alt: "people hiking at Torrey Pines",
+		position: { x: 14, y: 7, size: 4 },
+	},
 ].reverse();
 
 export default function HeroPhotos() {
@@ -39,7 +88,7 @@ export default function HeroPhotos() {
 			<div className={styles.blob} />
 			<div className={styles.gridWrapper}>
 				<div className={styles.grid}>
-					{photos.map(({ image, position }, i) => (
+					{photos.map(({ image, alt, position }, i) => (
 						<div
 							key={image.src}
 							className={styles.imageWrapper}
@@ -50,7 +99,16 @@ export default function HeroPhotos() {
 								animationDuration: `${i * 0.05 + 1}s`,
 							}}
 						>
-							<Image src={image} alt="TODO" fill />
+							<Image
+								src={image}
+								alt={alt}
+								width={50 * position.size}
+								height={50 * position.size}
+								// This is mostly to silence the NextJS warning about the big
+								// image contributing to LCP
+								priority={i === photos.length - 1}
+								className={styles.image}
+							/>
 						</div>
 					))}
 				</div>

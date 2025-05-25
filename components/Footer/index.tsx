3535e421e006a7351857755b512876240a265554
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2025-05-24 20:43:39 -0700

    Reorganize footer, fix layering issue in hero on mobile
    Resolves #17
    Fixes #16

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,23 +4,24 @@ export default function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.container}>
-				<a href="https://acmurl.com/cyber-discord">
-					<div className={styles["discord-logo"]} aria-label="Join our Discord server." />
+				<a href="http://acmucsd.com">
+					<div className={styles["acm-logo"]} aria-label="Go to the main ACM website." />
 				</a>
 				<a href="https://sdc.tf">
 					<div className={styles["sdctf-logo"]} aria-label="Learn more about SDCTF." />
 				</a>
-				<a href="https://acmurl.com/youtube">
-					<div className={styles["youtube-logo"]} aria-label="Visit our YouTube channel." />
+				<span className={styles.spacer} />
+				<a href="https://acmurl.com/cyber-discord">
+					<div className={styles["discord-logo"]} aria-label="Join our Discord server." />
 				</a>
 				<a href="https://www.instagram.com/cyber.ucsd/">
 					<div className={styles["instagram-logo"]} aria-label="Follow our Instagram." />
 				</a>
+				<a href="https://acmurl.com/youtube">
+					<div className={styles["youtube-logo"]} aria-label="Visit our YouTube channel." />
+				</a>
 				<a href="https://github.com/acmucsd/cyber-site">
 					<div className={styles["github-logo"]} aria-label="Leave suggestions on this website's GitHub repository." />
-				</a>
-				<a href="http://acmucsd.com">
-					<div className={styles["acm-logo"]} aria-label="Go to the main ACM website." />
 				</a>
 			</div>
 		</footer>
